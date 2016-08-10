d1da19398fe904a02d83c91b6e08c3406cc837f8
edsontm <edsontm@gmail.com>
2016-08-10 15:16:08 -0400

    versao melhorada

--- arena/detectposition.py
+++ arena/detectposition.py
@@ -13,6 +13,8 @@ class teste(unittest.TestCase):
     def teste1(self):
         obj = RobotPosition()
         print(obj.time.lista_robos())
+        frame = cv2.imread('../test.png')
+        obj.set_frame(frame)
         obj.find_robots()
         obj.find_ball()
 
@@ -20,9 +22,59 @@ class teste(unittest.TestCase):
 class RobotPosition():
     def __init__(self):
         self.time = robos.LIATime()
+        self.frame = None
+    def set_frame(self,frame):
+        self.frame = frame
+        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.dframe = cv2.adaptiveThreshold(self.gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
+
+
+        self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+
+    def _lista_candidatos(self,vcor):
+        ret = []
+        for robo in self.time.robos:
+            for cor in robo.icores:
+                cmin = cor[0]
+                cmax = cor[1]
+                if all(vcor>=cmin) and all(vcor <= cmax):
+                    ret.append(robo.id)
+        return ret
+
+
+
+
 
     def find_robots(self):
-        pass
+        img = self.hsv
+
+        for i in range(len(self.time.robos)):
+            robo = self.time.robos[i]
+            lres = []
+            for j in range(len(robo.icores)):
+                cor = robo.icores[j]
+                lower = cor[0]
+                upper = cor[1]
+
+                res = cv2.inRange(img,lower,upper)
+                #janela = 'res'+robo.nome+str(j)
+                #cv2.namedWindow(janela)
+                #cv2.imshow(janela, res)
+                lres.append(res)
+            if len(lres) > 1:
+                res = cv2.bitwise_or(lres[0],lres[1])
+            else:
+                res = lres[0]
+            janela = 'res' + robo.nome
+            cv2.namedWindow(janela)
+            cv2.imshow(janela, res)
+
+
+
+
+        key = cv2.waitKey(0) & 0xFF
+
+
     def find_ball(self):
         pass
 
--- arena/robos.py
+++ arena/robos.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class LIATime():
-    def __init__(self,nrobos = 3):
+    def __init__(self,nrobos = 4):
         self.nrobos = nrobos
         self.robos = []
         self._carrega_init()
@@ -11,21 +11,20 @@ class LIATime():
         f = csv.DictReader(open('init.csv'),delimiter =',')
         for row in f:
             obj = LIARobo(row['id'],row['nome'])
-            if row['cor1'] != None:
-                v = row['cor1'].strip('(').strip(')').split(':')
-                #print row['cor1'], v
-                obj.icores.append((int(v[0]),int(v[1])))
-            if row['cor2'] != None:
-                v = row['cor2'].strip('(').strip(')').split(':')
-                #print row['cor2'], v
-                obj.icores.append((int(v[0]),int(v[1])))
+            for tcor in ['cor1','cor2']:
+                if row[tcor] != None:
+                    v = row[tcor].strip('(').strip(')').split('#')
+                    v0 = v[0].split(':')
+                    v1 = v[1].split(':')
+                    tcormin = np.array([int(v0[0]),int(v0[1]),int(v0[2])])
+                    tcormax = np.array([int(v1[0]),int(v1[1]),int(v1[2])])
+                    obj.icores.append((tcormin,tcormax))
             self.robos.append(obj)
+
     def lista_robos(self):
         tstr = ''
         for robo in self.robos:
-            tstr += '%s %s'%(robo.id,robo.nome)
-            for cor in robo.icores:
-                tstr += ' (%d:%d) '%(cor[0],cor[1])
+            tstr += robo.salva_str()
             tstr += '\n'
         return tstr
     def _salva_init(self):
@@ -46,12 +45,27 @@ class LIARobo():
 
     def atualiza_icores(self):
         self.icores = []
+
         for lcor in self.lcores:
-             max = np.max(lcor)
-             min = np.min(lcor)
-             self.icores.append((min,max))
+            mcor = np.matrix(lcor)
+
+            maxh = np.max(mcor[:,0])
+            minh = np.min(mcor[:,0])
+
+            maxs = np.max(mcor[:,1])
+            mins = np.min (mcor[:, 1])
+
+            maxv = np.max(mcor[:,2])
+            minv = np.min (mcor[:, 2])
+
+            tcormin = np.array([minh, mins, minv])
+            tcormax = np.array([maxh,maxs,maxv])
+
+            self.icores.append((tcormin,tcormax))
     def salva_str(self):
         tstr = '%s,%s'%(self.id,self.nome)
         for cores in self.icores:
-            tstr += ',(%d:%d)'%(cores[0],cores[1])
+            c0 = cores[0]
+            c1 = cores[1]
+            tstr += ',(%d:%d:%d#%d:%d:%d)'%(c0[0],c0[1],c0[2],c1[0],c1[1],c1[2])
         return tstr
